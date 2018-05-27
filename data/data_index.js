841e859d444540f1586741a8ce8a6a9fe5070364
Evettey <37333287+Evettey@users.noreply.github.com>
2018-05-27 17:44:07 +0800

    数据

---
+++ data/data_index.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Rebecca_Han on 16/10/26.
+ */
+module.exports = {
+
+}
+
+var index= {
+    "id": 1,
+        "data": [
+        {
+            "question_id": 1,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "中国太极拳",
+            "feed_source_txt": "太极拳入门",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "answer_ctnt": "视频简介：太极拳入门课程主要是对太极拳的手、眼、身、法、步以及太极拳的十三势和太极拳活步功法的讲解。讲解详细，适合初学者学习观看。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 2,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "中国太极拳",
+            "feed_source_txt": "九式太极拳套路",
+            "feed_source_img": "../../images/icon8.jpg",
+            "answer_ctnt": "视频简介：“九式太极拳套路”是结合了陈式太极拳老架一路、老架二路套路中精选招式，根据招式的运动形式完美相结合。九式套路中体现了一路的柔劲、二路的刚劲，掤、捋、挤、按、采、挒、肘、靠，前后顾盼定，八法五步全在其中。学习好九式套路为以后学习传统套路可打下良好基础。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 3,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "武当太和拳",
+            "feed_source_txt": "武当太和拳",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "answer_ctnt": "视频简介：太和拳套路短小，极为简单朴实，如干枝老梅，具山野蓑衲之风，有桃源野鹤之态，全套共二十二式，盖取天干地支之合数（天干十数，地支十二数）焉，内含太极、阴阳、五行、八卦周流运化之理，以赞化育之道。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 4,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "剑术",
+            "feed_source_txt": "剑术",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "answer_ctnt": "剑素有“短兵之帅”的美称，其有身长、柄短、带尖、两面有刃，体轻、扁薄而有韧性等特点，使用灵活，且剑法也颇多。由于剑器扁薄轻巧，不利于正面进攻或硬挡硬架，故在实战中应采取“护中直刺，巧闪旁扼”的方法，达到“以巧制胜”之目的...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+          "question_id": 5,
+          "answer_id": 6,
+          "feed_source_id": 51,
+          "feed_source_name": "五禽戏",
+          "feed_source_txt": "五禽戏",
+          "feed_source_img": "../../images/icon8.jpg",
+          "answer_ctnt": "五禽戏，古代导引术。模仿虎、鹿、熊、猿、鸟的动作形态的一种导引方法。东汉末年著名医学家、养生家华佗创编。《三国志·魏志·华佗传》：“佗语普曰：吾有一术，名五禽之戏：一曰虎、二曰鹿、三曰熊、四曰猿、五曰鸟。亦以除疾，兼利蹄足，以当导引。体有不快，起作一禽之戏，怡汗出，因以著粉，身体轻便而得食。”",
+          "good_num": "112",
+          "comment_num": "18"
+        }
+
+    ]
+
+}
+
+module.exports.index = index;
